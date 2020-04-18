ebdd46f764748ac9d6abca214f52da108c430e3d
Abigail Stone <stone.abigail@gmail.com>
2020-04-18 08:29:46 -0400

    first commit

---
+++ simplecamera.py
@@ -0,0 +1,13 @@
+"""
+simple camera to make testing easier
+"""
+
+import sys
+import time
+
+from picamera import PiCamera
+
+from aiy.board import Board
+from aiy.leds import Color, Leds, Pattern, PrivacyLed
+from aiy.toneplayer import TonePlayer
+from aiy.vision.inference import CameraInference
