a1a2a23141747b73659ecfb17eb307c8615c54a7
Abigail Stone <stone.abigail@gmail.com>
2020-07-18 17:27:53 -0400

    fixed annotation

--- birdwatcher.py
+++ birdwatcher.py
@@ -23,28 +23,18 @@ boring = ["Meleagris gallopavo (Wild Turkey)", "background"]
 # because apparently the bird feeder looks like a turkey
 
 
-def main():
-
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-    parser.add_argument('--threshold', '-t', type=float, default=0.1,
-                        help='Classification probability threshold.')
-
-    parser.add_argument('--top_k', '-n', type=int, default=1,
-                        help='Max number of returned classes.')
-
-    parser.add_argument('--sparse', '-s', action='store_true', default=False,
-                        help='Use sparse tensors.')
-
-    parser.add_argument('--model', '-m', choices=('plants', 'insects', 'birds'),
-                        required=False, default='birds', help='Model to run.')
-
-    args = parser.parse_args()
+def write_observation(obs):
+    """
+    writes a timestamped observation to a .txt file
+    """
+    with open('birdimages/birdlist.txt', 'a') as birdlist:
+        birdlist.write(obs)
 
 
-    model_type = {'plants':  inaturalist_classification.PLANTS,
-                  'insects': inaturalist_classification.INSECTS,
-                  'birds':   inaturalist_classification.BIRDS}[args.model]
+def lookforbirds(model_type, thresh, topk, sparse):
+    """
+    Main driver code for inference and camera capture
+    """
 
     with contextlib.ExitStack() as stack:
         leds = stack.enter_context(Leds())
@@ -70,29 +60,51 @@ def main():
                 if len(inaturalist_classification.get_classes(result)) >= 1:
 
                     classes = inaturalist_classification.get_classes(result,
-                                                             top_k=args.top_k,
-                                                             threshold=args.threshold)
+                                                             top_k=topk,
+                                                             threshold=thresh)
 
                     for i, (label, score) in enumerate(classes):
 
-                        if(label not in boring):
+                        if(label not in boring): # filtering camera artifacts
                             print('Result %d: %s (prob=%f)' % (i, label, score))
 
                             # write observation to file
-                            birdlist = open("birdimages/birdlist.txt","a") #append mode
-                            birdlist.write(time.strftime("%y-%m-%d_%H-%M-%S")+" %s (prob=%f) \n" % (label, score))
-                            birdlist.close()
+                            obs = time.strftime("%y-%m-%d_%H-%M-%S") + " %s (prob=%f) \n" % (label, score)
+                            write_observation(obs)
 
+                            # get species info
                             id = label.split(' ')
 
                             # save picture
-                            camera.annotate_text(id[0]+' '+id[1]+' (prob='+int(score*100)+'%)')
+                            camera.annotate_text = id[0]+' '+id[1]+' (prob= %.2f)' % score
                             camera.capture("birdimages/"+time.strftime("%y%m%d_%H%M%S_")+id[0]+"_"+id[1]+".jpg")
                             time.sleep(20)
-                            
+
 
         camera.stop_preview()
 
 
 if __name__ == '__main__':
-    main()
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--threshold', '-t', type=float, default=0.1,
+                        help='Classification probability threshold.')
+
+    parser.add_argument('--top_k', '-n', type=int, default=1,
+                        help='Max number of returned classes.')
+
+    parser.add_argument('--sparse', '-s', action='store_true', default=False,
+                        help='Use sparse tensors.')
+
+    parser.add_argument('--model', '-m', choices=('plants', 'insects', 'birds'),
+                        required=False, default='birds', help='Model to run.')
+
+    args = parser.parse_args()
+
+
+    model_type = {'plants':  inaturalist_classification.PLANTS,
+                  'insects': inaturalist_classification.INSECTS,
+                  'birds':   inaturalist_classification.BIRDS}[args.model]
+
+    lookforbirds(model_type, args.threshold, args.top_k, args.sparse)
